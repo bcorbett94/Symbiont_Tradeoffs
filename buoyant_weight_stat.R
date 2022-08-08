a14fbd3161490cc8e948dbf947c127906d581c77
Bryce Corbett <brycecorbett2@gmail.com>
2022-08-08 14:01:19 -0500

    First commit msg

--- buoyant_weight_stat.R
+++ buoyant_weight_stat.R
@@ -2,17 +2,20 @@ library(tidyverse)
 symdat<-read_csv("Data/Buoyant_Weight/symdat.csv")
 
 bw_mastersheet <- read_csv("Data/Buoyant_Weight/bw_mastersheet.csv") %>%
-  mutate(species = str_sub(FragID, 1, 1)) %>%
+  mutate(species = str_sub(FragID, 1, 1),
+         colony = str_sub(FragID, 1, 2)) %>%
   full_join(symdat)
 
 
 df <- bw_mastersheet %>%
-  group_by(FragID, sym, species) %>%
+  group_by(FragID, sym,colony, species) %>%
   summarise(perc_change = (weight[TimePoint == 2] -weight[TimePoint == 1])/(weight[TimePoint == 1])*100)
 #group_By<- tidyverse function that uses column names (no spaces, lower case etc), to make certain cgoups 
 df %>%
-  filter(species == "P") %>%
-  ggplot(aes(x=sym, y=perc_change))+geom_point()
+  filter(species == "T") %>%
+  ggplot(aes(x=colony, y=perc_change, color = sym))+geom_point()
 
 
-#
+df
+mod<-lm(perc_change ~ sym+colony, data = filter(df, species == "T"))
+anova(mod)
